7e90631ca301237c569df8c2b21688d706328022
Sub-zero745 <Subzero70000000@gmail.com>
2025-05-28 00:01:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-const target = 'ws://5.34.178.157:2086';
+const target = 'ws://207.174.40.206:80';
 
 const proxy = httpProxy.createProxyServer({
   target,
@@ -11,13 +11,17 @@ const proxy = httpProxy.createProxyServer({
 
 const server = http.createServer((req, res) => {
   proxy.web(req, res, (err) => {
+    console.error("Error proxy.web:", err);
     res.writeHead(502);
     res.end("Bad gateway: " + err.message);
   });
 });
 
 server.on('upgrade', (req, socket, head) => {
-  proxy.ws(req, socket, head);
+  proxy.ws(req, socket, head, (err) => {
+    console.error("Error proxy.ws:", err);
+    socket.destroy();
+  });
 });
 
 const PORT = process.env.PORT || 8080;
